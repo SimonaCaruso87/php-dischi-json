8448b340b7638c721f2eabac34bbab5d7660d65f
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-08-24 15:29:35 +0200

    inizializzazione esercizio

---
+++ api.php
@@ -0,0 +1,35 @@
+<?php 
+
+$dischi = [
+    [
+        'nome_artista' => 'Colapesce' ,
+        'titolo_canzone' => 'Restiamo a casa' ,
+        'img' => 'https://images.genius.com/d2c41736bebbf132befd28a17136bca9.1000x1000x1.jpg'
+    ],
+    [
+        'nome_artista' => 'Colapesce' ,
+        'titolo_canzone' => 'Restiamo a casa' ,
+        'img' => 'https://images.genius.com/d2c41736bebbf132befd28a17136bca9.1000x1000x1.jpg'
+    ],
+    [
+        'nome_artista' => 'Colapesce' ,
+        'titolo_canzone' => 'Restiamo a casa' ,
+        'img' => 'https://images.genius.com/d2c41736bebbf132befd28a17136bca9.1000x1000x1.jpg'
+    ],
+    [
+        'nome_artista' => 'Colapesce' ,
+        'titolo_canzone' => 'Restiamo a casa' ,
+        'img' => 'https://images.genius.com/d2c41736bebbf132befd28a17136bca9.1000x1000x1.jpg'
+    ],
+    [
+        'nome_artista' => 'Colapesce' ,
+        'titolo_canzone' => 'Restiamo a casa' ,
+        'img' => 'https://images.genius.com/d2c41736bebbf132befd28a17136bca9.1000x1000x1.jpg'
+    ],
+
+    
+] ;
+
+header('Content-Type : application/json');
+
+echo json_encode($dischi);
