57daf04172500464f533c0690e99caf76da6249a
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-08-25 11:09:19 +0200

    edit file index

--- api.php
+++ api.php
@@ -7,13 +7,13 @@ header("Access-Control-Allow-Origin: *" );
 // header("Access-Control-Allow-Headers : X-Requested");
 
 //1°cosa : fare far leggere il contenuto del file e farcelo restituire come stringa
-$string = file_get_contents('./database/api.json');
+$dischi = file_get_contents('./database/api.json');
 
 //3°mettere una condizione per farci restituire i dati in pagina
 //se nome artista è settato filtro altrimenti me li restituisce tutti 
 if (isset($_GET['id_song'])){
-     //Filtro i dati , non posso filtrare in un array associativo quindi lo trasformo in stringa
-     $dataArrayApi = json_decode($string, true);
+     //Filtro i dati , non posso filtrare in un array associativo quindi lo trasformo in dischi
+     $dataArrayApi = json_decode($dischi, true);
      //creiamo un array vuoto
      $responseSingleSong = []; 
 
@@ -33,8 +33,10 @@ else{
     //2° cosa da fare : restituisco il contenuto del file, setto l'header e lo restituisco
     header('Content-Type:application/json');
 
-    echo $string;
+    echo $dischi;
 }
+
+
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,19 @@ const { createApp } = Vue
     createApp({
         data(){
             return{
-                message : 'Hello Vue'
+                dischi : []
             }
         },
         //qui facciamo la nostra chiamara api
         created(){
             // console.log('chiamata api');
+            //chiamata a un url php
             axios
                  .get('http://localhost/php-dischi-json/api.php')
                  .then(res => {
-                    console.log(res.data)
+                    // console.log(res.data)
+                    this.dischi = res.data;
                  });
+
         }
     }).mount('#app')
